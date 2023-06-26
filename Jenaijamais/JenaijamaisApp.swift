f87ff49d79f77a23ba2e2ea48b2db5ede59d093a
Cindoushka <cindie-baj@live.fr>
2023-06-26 13:51:26 +0200

    Initial Commit

---
+++ Jenaijamais/JenaijamaisApp.swift
@@ -0,0 +1,17 @@
+//
+//  JenaijamaisApp.swift
+//  Jenaijamais
+//
+//  Created by Cindy Bajoni on 26/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct JenaijamaisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
